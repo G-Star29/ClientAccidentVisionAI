c75229e0c27dc0a56288ed08dc1418363e26e611
G-Star29 <37033232+G-Star29@users.noreply.github.com>
2024-05-22 02:41:31 +0700

    Add weather card and stati

--- script.js
+++ script.js
@@ -76,4 +76,29 @@ function init() {
 
     // Вызов функции для получения и отображения данных при загрузке страницы
     getCoordinates('http://127.0.0.1:8000/current-situation');
+    fetchWeatherData();
+}
+
+// Функция для получения данных о погоде
+async function fetchWeatherData() {
+    try {
+        const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=55.0415&longitude=82.9346&current=temperature_2m,relative_humidity_2m,is_day,precipitation,rain,showers,snowfall,weather_code,wind_speed_10m&timezone=auto');
+        const data = await response.json();
+        displayWeatherData(data.current);
+    } catch (error) {
+        console.error('Ошибка при получении данных о погоде:', error);
+    }
+}
+
+// Функция для отображения данных о погоде
+function displayWeatherData(weather) {
+    document.getElementById('temperature').textContent = `${weather.temperature_2m} °C`;
+    document.getElementById('humidity').textContent = `${weather.relative_humidity_2m} %`;
+    document.getElementById('day-night').textContent = weather.is_day ? 'День' : 'Ночь';
+    document.getElementById('precipitation').textContent = `${weather.precipitation} мм`;
+    document.getElementById('rain').textContent = `${weather.rain} мм`;
+    document.getElementById('showers').textContent = `${weather.showers} мм`;
+    document.getElementById('snowfall').textContent = `${weather.snowfall} см`;
+    document.getElementById('weather-code').textContent = weather.weather_code;
+    document.getElementById('wind-speed').textContent = `${weather.wind_speed_10m} км/ч`;
 }
